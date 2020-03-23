5b02b069efd35604c1b0345fdb0d9d4a7ea04748
kee <18316603168@163.com>
2020-03-23 16:47:30 +0800

    Refine test procedures“

---
+++ data.py
@@ -0,0 +1,5 @@
+
+class DataTearDown(object):
+    def __init__(self, variables=None):
+        self.variable = variables
+        self.validation_results = []
--- runner.py
+++ runner.py
@@ -1,7 +1,7 @@
-
-from utils  import  py_version_2
+from serunner.log  import logger
+from serunner.utils  import  py_version_2
 import inspect
-from data import DataTearDown
+from serunner.data import DataTearDown
 class Runner():
     def __init__(self,each_test_global):
         self.test_global = DataTearDown(each_test_global)
@@ -59,7 +59,7 @@ class Runner():
                     new_kwargs[args[index]] = func_arg
         new_kwargs.update(kwargs)
         return new_kwargs
-    def _get_static_args(self,func,funcArgs):
+    def __get_static_args(self,func,funcArgs):
         '''
         :param func:
         :param funcArgs:
@@ -71,23 +71,57 @@ class Runner():
         kwargs = self._get_func_args(defaults,args,funcArgs)
         return  kwargs
 
-    def run_keyword(self,func,funcArgs):
+    def __run_keyword(self,func,funcArgs):
         '''
         运行关键词函数
         :param func:
         :param funcArgs:
         :return:
         '''
-        kwargs = self._get_static_args(func,funcArgs)
+        kwargs = self.__get_static_args(func,funcArgs)
         result = func(**kwargs)
         return result
 
-    def run_test(self,steps):
-        for step in steps:
+
+    def  run_test(self,test_case_dict):
+        '''
+        运行用例
+        '''
+        # 提取用例信息
+        run_step = test_case_dict.get("steps",[])
+        fact_result = test_case_dict.get("fact_result",[])
+        assert_assert = test_case_dict.get("assert",[])
+
+        def run_method(value):
+            # 运行关键字
+            if len(value) == 1:
+                result = self.__run_keyword(value[0][0], value[0][1])
+            else:
+                for index in range(len(value)):
+                    result = self.__run_keyword(value[index][0], value[index][1])
+
+            return result
+
+        # 运行测试步骤
+        fact_result_list,each_result = [],{}
+        for step in run_step:
             for key,value in step.items():
-                if len(value) == 1:
-                    self.run_keyword(value[0][0],value[0][1])
+                # 提取结果
+                if key in fact_result:
+                    result = run_method(value)
+                    each_result[key] = result
                 else:
-                    for index in range(len(value)):
-                        self.run_keyword(value[index][0],value[index][1])
+                    run_method(value)
+        logger.info("步骤运行成功")
+        fact_result_list.append(each_result)
+
+        # 更新全局变量
+        self.test_global.update_variable(fact_result_list)
+
+        # 结果验证
+        self.test_global.assert_result(assert_assert)
+
+
+
+
 
