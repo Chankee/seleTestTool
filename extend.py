46b82e2cfa676e2f3fe10c4918c43ce122d89968
kee <18316603168@163.com>
2020-03-30 11:25:56 +0800

    Modify the extension module

--- extend.py
+++ extend.py
@@ -1,9 +1,8 @@
-from sekey import _get_global
+
 
 class Extend():
     def __init__(self,target):
         self.target=target
 
     def my_input(self,name,text):
-        print (self.target)
         self.target.find_element_by_id(name).send_keys(text)
